dc82bcca0235cddb2e1369b9c15481849644aeaa
manishpal <manishkumar9306074325@gmail.com>
2024-10-03 15:51:57 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,10 @@ import Timepass from "./Components/Timepass";
 import { Works } from "./Components/Works";
 import Bg from "./assets/Bg.png"
 import { My_Stack } from "./Components/My_Stack";
-import Spline from "@splinetool/react-spline";
 
 function App() {
   return (
     <div className="h-screen w-full  bg-black">
-      <div className="absolute w-full h-full my-1/2">
-      {/* Spline component */}
-      <Spline scene="https://prod.spline.design/e6E9vGTn1iavPd1p/scene.splinecode" />
-      </div>
-      <img className="absolute " src={Bg} alt="" />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} /> {/* Home Route */}
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import Spline from "@splinetool/react-spline";
+
 
 const Home = () => {
   return (
-    <div className= "absolute flex-col top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2  w-full md:w-1/4">
-    <h1 className=" py-7 text-4xl text-white">
-      Designer | Developer
-    </h1>
-    <p className=" text-white/30">
-      A designer-developer is skilled in both design and coding. They
-      create attractive, easy-to-use websites or apps and make sure they
-      work properly. This combination helps them build digital experiences
-      that look good and function well.
-    </p>
-  </div>
+    <>  
+    <div className="flex justify-center items-center"> 
+    
+     <div className=" w-1/2 h-screen p-8 flex-col justify-center ">
+      <h1 className="text-white font-black text-5xl py-10 flex-col justify-center mx-auto" >Designer | Developer</h1>
+       <p className=" text-white/30 flex-col justify-center">
+        A designer-developer is skilled in both design and coding. They create
+        attractive, easy-to-use websites or apps and make sure they work
+        properly. This combination helps them build digital experiences that
+        look good and function well.
+      </p>
+     </div>
+     <div className="w-1/2 h-screen ">
+       <Spline scene="https://prod.spline.design/e6E9vGTn1iavPd1p/scene.splinecode" />
+      </div>
+     </div>
+    </>
+
   );
 };
 
